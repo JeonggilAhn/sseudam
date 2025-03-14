1e45cc0085a1c3a7ee65769c825efa05dc06168e
안정길 <jeonggilahn@gmail.com>
2025-03-14 14:36:58 +0900

    chore : base settings

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -32,7 +32,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <ReduxProvider>{children}</ReduxProvider>
       </body>
     </html>
