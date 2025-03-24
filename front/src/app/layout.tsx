780d8126e3ca6f73f15f3f1b9ae51ebf87557425
안정길 <jeonggilahn@gmail.com>
2025-03-24 12:51:24 +0900

    ⚙️ chore : add underbar

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ReduxProvider from "./provider";
+import UnderBar from "@/components/underBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +34,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ReduxProvider>{children}</ReduxProvider>
+        <ReduxProvider>
+          <div className="pb-16">{children}</div>
+          <UnderBar />
+        </ReduxProvider>
       </body>
     </html>
   );
--- front/src/components/Icon.tsx
+++ front/src/components/Icon.tsx
@@ -8,7 +8,7 @@ type IconProps = {
   color?: string; //svg 파일에 따라 적용이 안 될 수 있음
 };
 
-const Icon = ({ name, width = 45, height = 15, alt = "", isActive = true, color }: IconProps) => {
+const Icon = ({ name, width = 60, height = 20, alt = "", isActive = true, color }: IconProps) => {
   const src = `/icons/${name}.svg`;
 
   return (
