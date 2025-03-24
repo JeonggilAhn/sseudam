c7f59c1d607f0f72378d32369d0963acc84971c6
harimau97 <harimauho@gmail.com>
2025-03-24 23:24:15 +0900

    fix : mainPage 반응형 버그 수정

--- front/src/app/card/components/grassBackground.tsx
+++ front/src/app/card/components/grassBackground.tsx
@@ -1,7 +1,7 @@
 const GrassBackground = () => {
   return (
     <div className="absolute bottom-0 left-0 w-full h-full overflow-hidden">
-      <div className="z-[120] w-[150vw] h-[80vh] rounded-t-full bg-[#62B84A] -translate-x-[17.5%] translate-y-[40%]"></div>
+      <div className="z-[120] w-full h-[80vh] rounded-t-full bg-[#62B84A] translate-y-[35%]"></div>
     </div>
   );
 };
