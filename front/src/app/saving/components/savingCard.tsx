21013fad94aa8738f9c53495e7ffd8ee302bd402
안정길 <jeonggilahn@gmail.com>
2025-03-25 13:25:57 +0900

    ⚙️ chore : add savingDetails #S12P21A106-266

--- front/src/app/saving/components/savingCard.tsx
+++ front/src/app/saving/components/savingCard.tsx
@@ -5,20 +5,27 @@ import { Eye, Heart } from "lucide-react";
 import Icon from "@/components/Icon";
 import { getBankIconName } from "@/components/bankList";
 
-const SavingCard: React.FC = () => {
+type Props = {
+  onClickJoin: () => void;
+};
+
+const SavingCard: React.FC<Props> = ({ onClickJoin }) => {
   return (
     <div className="bg-white rounded-xl border border-[#0074D9] px-4 py-3 w-full max-w-3xl mx-auto shadow flex items-center justify-between mb-4">
       <div className="flex items-center gap-2 min-w-[100px]">
-        <Icon name={getBankIconName("싸피은행")} width={120} height={40} />
+        <Icon name={getBankIconName("0010001")} width={120} height={40} />
       </div>
 
       <div className="flex flex-col items-center text-center flex-1">
-        <div className="font-bold text-lg">WON적금</div>
-        <div className="text-sm">4 % ~ 6 %</div>
+        <div className="font-bold text-lg">적금 이름</div>
+        <div className="text-sm">최저 금리% ~ 최고 금리 %</div>
       </div>
 
       <div className="flex flex-col items-end gap-2">
-        <button className="bg-[#60B94D] hover:bg-green-600 font-bold text-sm px-3 py-1 border border-black shadow-sm cursor-pointer">
+        <button
+          className="bg-[#60B94D] hover:bg-green-600 text-white font-bold text-sm px-3 py-1 rounded border   cursor-pointer"
+          onClick={onClickJoin}
+        >
           가입하기
         </button>
         <div className="flex items-center gap-3 text-sm">
