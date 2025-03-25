0945dd9ef37db51a5649bff7c45d5ab674c03abd
안정길 <jeonggilahn@gmail.com>
2025-03-25 17:54:15 +0900

    ⚙️ chore : add saving details #S12P21A106-272

--- front/src/components/Icon.tsx
+++ front/src/components/Icon.tsx
@@ -1,11 +1,12 @@
-// Icon.tsx
+import Image from "next/image";
+
 type IconProps = {
   name: string;
   width?: number;
   height?: number;
   alt?: string;
   isActive?: boolean;
-  color?: string; //svg 파일에 따라 적용이 안 될 수 있음
+  color?: string; // svg 파일에 따라 적용이 안 될 수 있음
 };
 
 const Icon = ({
@@ -18,19 +19,25 @@ const Icon = ({
 }: IconProps) => {
   const src = `/icons/${name}.svg`;
 
+  const customStyle: React.CSSProperties = {
+    filter: isActive ? "none" : "grayscale(100%)",
+  };
+
+  // color 필터가 있다면 기존 필터에 덮어씀
+  if (color) {
+    customStyle.filter = `invert(1) sepia(1) saturate(5) hue-rotate(${color})`;
+  }
+
   return (
-    <img
-      src={src}
-      alt={alt || name}
-      width={width}
-      height={height}
-      style={{
-        filter: isActive ? "none" : "grayscale(100%)",
-        ...(color
-          ? { filter: `invert(1) sepia(1) saturate(5) hue-rotate(${color})` }
-          : {}),
-      }}
-    />
+    <div style={{ width, height, position: "relative", ...customStyle }}>
+      <Image
+        src={src}
+        alt={alt || name}
+        fill
+        sizes={`${width}px`}
+        style={{ objectFit: "contain" }}
+      />
+    </div>
   );
 };
 
