31e44f2404f12aeeef78285be7d4710dcb28bdad
이시호 <harimauho@gmail.com>
2025-03-26 17:52:34 +0900

    ✨ feat : 카드 dto 및 repository 추가

---
+++ back/ssdam/src/main/java/elevenjo/ssdam/domain/card/dto/CardDto.java
@@ -0,0 +1,14 @@
+package elevenjo.ssdam.domain.card.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CardDto {
+    private String cardNo;
+    private String cvc;
+    private int user;
+}
