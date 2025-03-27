4f15e6935e75a6f85fb5efc174b8da0300dcf586
조현석 <hsjo0227@gmail.com>
2025-03-27 13:10:11 +0900

    ✨ feat : 유저 crud추가 #S12P21A106-364

--- back/ssdam/src/main/java/elevenjo/ssdam/domain/user/controller/UserController.java
+++ back/ssdam/src/main/java/elevenjo/ssdam/domain/user/controller/UserController.java
@@ -1,14 +1,14 @@
 package elevenjo.ssdam.domain.user.controller;
 
+import elevenjo.ssdam.global.externalApi.ExternalApiUtil;
+import elevenjo.ssdam.domain.user.dto.RegisterUserInfoRequestDto;
+import elevenjo.ssdam.domain.user.dto.UpdateUserRequestDto;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import elevenjo.ssdam.domain.user.dto.UserDto;
 import elevenjo.ssdam.domain.user.entity.User;
@@ -17,36 +17,50 @@ import elevenjo.ssdam.global.response.ResponseWrapper;
 import elevenjo.ssdam.global.response.ResponseWrapperFactory;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
-//	private final UserService userService;
-//
-//	@Autowired
-//	public UserController(UserService userService) {
-//		this.userService = userService;
-//	}
-//
-//	@GetMapping("/users/me")
-//	public ResponseEntity<ResponseWrapper<UserDto>> getSelf(
-//		@AuthenticationPrincipal User user
-//	) {
-//		return ResponseWrapperFactory.setResponse(
-//			HttpStatus.OK,
-//			null,
-//			userService.user(user.getUserId())
-//		);
-//	}
-//
-//	@GetMapping("/users/{userId}")
-//	public ResponseEntity<ResponseWrapper<UserDto>> getUser(
-//		@PathVariable("userId") Long userId
-//	) {
-//		return ResponseWrapperFactory.setResponse(
-//			HttpStatus.OK,
-//			null,
-//			userService.user(userId)
-//		);
-//	}
-//
+	private final UserService userService;
+
+	@Autowired
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
+	@GetMapping("/me")
+	public ResponseEntity<ResponseWrapper<UserDto>> getSelf(
+		@AuthenticationPrincipal User user
+	) {
+		return ResponseWrapperFactory.setResponse(
+			HttpStatus.OK,
+			null,
+			userService.user(user.getUserId())
+		);
+	}
+
+	@PostMapping("/me")
+	public ResponseEntity<ResponseWrapper<UserDto>> registerSelfInfo(
+			@AuthenticationPrincipal User user,
+			@Valid @RequestBody RegisterUserInfoRequestDto requestDto
+	) {
+		return ResponseWrapperFactory.setResponse(
+				HttpStatus.OK,
+				null,
+				userService.registerUserInfo(user.getUserId(), requestDto)
+		);
+	}
+
+	@PatchMapping("/me")
+	public ResponseEntity<ResponseWrapper<UserDto>> updateSelf(
+			@AuthenticationPrincipal User user,
+			@Valid @RequestBody UpdateUserRequestDto requestDto
+	) {
+		return ResponseWrapperFactory.setResponse(
+				HttpStatus.OK,
+				null,
+				userService.updateUser(user.getUserId(), requestDto)
+		);
+	}
+
 //	@PreAuthorize("@authExpression.isSelf(#userId)")
 //	@DeleteMapping("/users/{userId}")
 //	public ResponseEntity<ResponseWrapper<Void>> deleteUser(
