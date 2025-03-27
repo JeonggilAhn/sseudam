6ec3b6e7d1fb3dfbf01312e980a08c4433283921
이시호 <harimauho@gmail.com>
2025-03-27 10:58:25 +0900

    🚧 wip : createCard in CardController

--- back/ssdam/src/main/java/elevenjo/ssdam/domain/card/controller/CardController.java
+++ back/ssdam/src/main/java/elevenjo/ssdam/domain/card/controller/CardController.java
@@ -1,17 +1,22 @@
 package elevenjo.ssdam.domain.card.controller;
 
 
+import elevenjo.ssdam.global.ssafyApi.SsafyApiUtil;
 import lombok.RequiredArgsConstructor;
 import elevenjo.ssdam.domain.card.dto.CardDto;
 import elevenjo.ssdam.domain.card.service.CardService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("")
 public class CardController {
     private final CardService cardService;
+    private final SsafyApiUtil externalApiUtil;
 
     @PostMapping("/card")
     public ResponseEntity<Void> registerCard(@RequestBody CardDto card){
@@ -20,8 +25,9 @@ public class CardController {
     };
 
     @PostMapping("/cards")
-    public ResponseEntity<Void> createCard(@RequestBody CardDto card){
+    public <UserCardInfo> ResponseEntity<Void> createCard(@AuthenticationPrincipal User user, @RequestBody Map<String, Object> userCardInfo){
 
+        externalApiUtil.postWithHeader("https://finopenapi.ssafy.io/ssafy/api/v1/edu/creditCard/createCreditCard","createCreditCard", "",userCardInfo, String);
         return ResponseEntity.ok().build();
     }
 
