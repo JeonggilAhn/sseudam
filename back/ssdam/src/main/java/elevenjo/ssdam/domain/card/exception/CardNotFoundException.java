9b571334abc4098d668c4bd2f2bafd75ab554851
최휘준 <chjcoder@gmail.com>
2025-03-27 12:49:53 +0900

    ✨ feat : 기간별 카드 사용 내역 api #S12P21A106-345

---
+++ back/ssdam/src/main/java/elevenjo/ssdam/domain/card/exception/CardNotFoundException.java
@@ -0,0 +1,9 @@
+package elevenjo.ssdam.domain.card.exception;
+
+import elevenjo.ssdam.global.exception.BaseRuntimeException;
+import elevenjo.ssdam.global.exception.CustomException;
+import elevenjo.ssdam.global.exception.ExceptionCode;
+
+@CustomException(ExceptionCode.CARD_NOT_FOUND)
+public class CardNotFoundException extends BaseRuntimeException {
+}
---
+++ back/ssdam/src/main/java/elevenjo/ssdam/domain/cardTransaction/dto/CardTransactionRequestDto.java
@@ -0,0 +1,7 @@
+package elevenjo.ssdam.domain.cardTransaction.dto;
+
+public record CardTransactionRequestDto(
+    String startDate,
+    String endDate
+) {
+}
