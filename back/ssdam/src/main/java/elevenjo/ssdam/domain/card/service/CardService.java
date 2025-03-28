d5289aa8e09e692b9ded3184c6ec57f798b686f5
이시호 <harimauho@gmail.com>
2025-03-28 16:38:45 +0900

    ⚙️ chore : 카드 유효성 검사 로직 수정

--- back/ssdam/src/main/java/elevenjo/ssdam/domain/card/service/CardService.java
+++ back/ssdam/src/main/java/elevenjo/ssdam/domain/card/service/CardService.java
@@ -5,7 +5,7 @@ import elevenjo.ssdam.domain.card.dto.CardDto;
 import java.util.List;
 
 public interface CardService {
-    void registerUserCard(CardDto userCard);
+    void registerUserCard(CardDto userCard) throws Exception;
     void deleteUserCard(Long cardId);
     CardDto getUserCard(long userId) throws Exception;
 }
