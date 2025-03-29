834783ce79ece73f2c1c5b9a3a8d0e636d176af5
chj <chjcoder@gmail.com>
2025-03-29 22:54:33 +0900

    ✨ feat : 유저에게 쿠폰 발행해주는 api 작성 #S12P21A106-346

--- back/ssdam/src/main/java/elevenjo/ssdam/domain/coupon/service/CouponService.java
+++ back/ssdam/src/main/java/elevenjo/ssdam/domain/coupon/service/CouponService.java
@@ -1,15 +1,25 @@
 package elevenjo.ssdam.domain.coupon.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import elevenjo.ssdam.domain.coupon.dto.request.CouponCreateRequestDto;
+import elevenjo.ssdam.domain.coupon.dto.request.CouponIssueRequestDto;
 import elevenjo.ssdam.domain.coupon.dto.response.CouponCreateResponseDto;
+import elevenjo.ssdam.domain.coupon.dto.response.CouponIssueResponseDto;
 import elevenjo.ssdam.domain.coupon.entity.Coupon;
+import elevenjo.ssdam.domain.coupon.entity.CouponIssued;
+import elevenjo.ssdam.domain.coupon.exception.CouponExpiredException;
+import elevenjo.ssdam.domain.coupon.exception.CouponNotFoundException;
+import elevenjo.ssdam.domain.coupon.exception.CouponOutOfStockException;
+import elevenjo.ssdam.domain.coupon.repository.CouponIssuedRepository;
 import elevenjo.ssdam.domain.coupon.repository.CouponRepository;
 import elevenjo.ssdam.domain.saving.entity.Saving;
 import elevenjo.ssdam.domain.saving.exception.SavingNotFoundException;
 import elevenjo.ssdam.domain.saving.repository.SavingRepository;
+import elevenjo.ssdam.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -18,6 +28,7 @@ public class CouponService {
 
     private final CouponRepository couponRepository;
     private final SavingRepository savingRepository;
+    private final CouponIssuedRepository couponIssuedRepository;
 
     @Transactional
     public CouponCreateResponseDto create(CouponCreateRequestDto requestDto) {
@@ -29,4 +40,33 @@ public class CouponService {
 
         return CouponCreateResponseDto.of(coupon, saving);
     }
+
+    @Transactional
+    public CouponIssueResponseDto issueCoupon(CouponIssueRequestDto requestDto, User user) {
+        Coupon coupon = couponRepository.findById(requestDto.couponId())
+                .orElseThrow(CouponNotFoundException::new);
+
+        validateCouponNotExpired(coupon);
+        validateCouponStockAvailable(coupon);
+
+        coupon.decreaseCouponCnt();
+
+        CouponIssued couponIssued = CouponIssued.of(user, coupon);
+        couponIssuedRepository.save(couponIssued);
+
+        return CouponIssueResponseDto.of(couponIssued);
+    }
+
+    private void validateCouponNotExpired(Coupon coupon) {
+        if (coupon.getCouponDeadline().isBefore(LocalDateTime.now())) {
+            throw new CouponExpiredException(); // 필요 시 커스텀 예외
+        }
+    }
+
+    private void validateCouponStockAvailable(Coupon coupon) {
+        if (coupon.getCouponCnt() == null || coupon.getCouponCnt() <= 0) {
+            throw new CouponOutOfStockException();
+        }
+    }
+
 }
