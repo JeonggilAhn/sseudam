8f569bc21cdb700b24529c83441cc3af9b743139
최휘준 <chjcoder@gmail.com>
2025-04-01 17:22:39 +0900

    🐛 fix : entity_column 이름 수정 #S12P21A106-482

--- back/ssdam/src/main/java/elevenjo/ssdam/domain/cardTransaction/entity/CardTransaction.java
+++ back/ssdam/src/main/java/elevenjo/ssdam/domain/cardTransaction/entity/CardTransaction.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -42,6 +43,7 @@ public class CardTransaction {
     private Long transactionBalance;
 
     @ManyToOne
+    @JoinColumn(name = "user_id")
     private Card card;
 
     @Builder
--- back/ssdam/src/main/java/elevenjo/ssdam/domain/coupon/entity/CouponIssued.java
+++ back/ssdam/src/main/java/elevenjo/ssdam/domain/coupon/entity/CouponIssued.java
@@ -10,6 +10,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -40,8 +41,11 @@ public class CouponIssued extends BaseTimeEntity {
     private LocalDateTime usedAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
+
     @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "coupon_id")
     private Coupon coupon;
 
     @Builder
