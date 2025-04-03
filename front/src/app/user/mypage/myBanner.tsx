05bbf7bf8ec51eaf5b07467b01d767aaef85fb92
yw <sapientia268@gmail.com>
2025-04-03 18:02:13 +0900

    ✨ feat : myPage component 추가 #S12P21A106-450

---
+++ front/src/app/user/mypage/myBanner.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const MyBanner1 = () => {
+  return (
+    <>
+      <div>
+        <div className="w-50 h-60 bg-[#FCF1CD]">이름 생년월일 이메일</div>
+      </div>
+    </>
+  );
+};
+
+export const MyBanner2 = () => {
+  return (
+    <>
+      <div>저축 비율 설정 알림</div>
+    </>
+  );
+};
--- front/src/app/user/page.tsx
+++ front/src/app/user/page.tsx
@@ -1,1 +1,18 @@
 import React from "react";
+import PageSetting from "../pageSetting";
+import { MyBanner1, MyBanner2 } from "./mypage/myBanner";
+
+const UserInfo = () => {
+  return (
+    <>
+      <PageSetting headerLink="/" pageTitle="마이페이지">
+        개인정보
+        <MyBanner1 />
+        설정
+        <MyBanner2 />
+      </PageSetting>
+    </>
+  );
+};
+
+export default UserInfo;
