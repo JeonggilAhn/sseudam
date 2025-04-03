a4000a6b4d2e9d394cae5c87553c243de612733c
이승연 <snowball@sookmyung.ac.kr>
2025-04-03 15:26:58 +0900

    ✨ feat : env 로드 메서드 호출 추가 #S12P21A106-449

--- back/ssdam/src/main/java/elevenjo/ssdam/SsdamApplication.java
+++ back/ssdam/src/main/java/elevenjo/ssdam/SsdamApplication.java
@@ -5,12 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
 public class SsdamApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(SsdamApplication.class, args);
 	}
 
