af09e1426903c874065233b275bfb79d152d3815
이시호 <harimauho@gmail.com>
2025-04-04 10:15:07 +0900

    👺 hotfix : 로그인 리다이렉트 오류수정

--- front/src/app/auth-redirect/page.tsx
+++ front/src/app/auth-redirect/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { UserAuth } from "@/utils/userAuth";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
@@ -26,21 +27,21 @@ const RedirectPage = () => {
     }, 1000);
 
     // 인증 처리
-    // const timer = setTimeout(() => {
-    //   const handleAuth = async () => {
-    //     try {
-    //       await UserAuth();
-    //       // 인증 성공 후 메인 페이지로 이동
-    //       setTimeout(() => {
-    //         router.push("/card");
-    //       }, 2000);
-    //     } catch (err) {
-    //       setIsError(true);
-    //     }
-    //   };
+    const timer = setTimeout(() => {
+      const handleAuth = async () => {
+        try {
+          await UserAuth();
+          // 인증 성공 후 메인 페이지로 이동
+          setTimeout(() => {
+            router.push("/card");
+          }, 2000);
+        } catch (err) {
+          setIsError(true);
+        }
+      };
 
-    //   handleAuth();
-    // }, 200);
+      handleAuth();
+    }, 200);
 
     return () => {
       clearInterval(dotsInterval);
