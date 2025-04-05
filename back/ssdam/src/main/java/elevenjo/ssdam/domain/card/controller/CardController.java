ab8d89f8235cd0d9d7c202795e8e7572246f7921
harimau97 <harimauho@gmail.com>
2025-04-05 11:03:14 +0900

    👺 hotfix : edit card controller

--- back/ssdam/src/main/java/elevenjo/ssdam/domain/card/controller/CardController.java
+++ back/ssdam/src/main/java/elevenjo/ssdam/domain/card/controller/CardController.java
@@ -26,7 +26,7 @@ public class CardController {
     private final CardService cardService;
     private final ExternalApiUtil externalApiUtil;
 
-    @PostMapping("/")
+    @PostMapping("/regist")
     public ResponseEntity<ResponseWrapper<Void>> registerCard(@RequestBody CardDto card, @AuthenticationPrincipal User user) throws Exception {
             cardService.registerUserCard(card, user);
             return ResponseWrapperFactory.setResponse(DefaultResponseCode.OK,null);
