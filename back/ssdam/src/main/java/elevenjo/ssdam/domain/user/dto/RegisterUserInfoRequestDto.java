18320f0b402543e2923020dbb9f98efb5aa27d66
조현석 <hsjo0227@gmail.com>
2025-04-08 16:14:28 +0900

    🐛 fix : validation 의존성 삭제 #S12P21A106-587

--- back/ssdam/src/main/java/elevenjo/ssdam/domain/user/dto/RegisterUserInfoRequestDto.java
+++ back/ssdam/src/main/java/elevenjo/ssdam/domain/user/dto/RegisterUserInfoRequestDto.java
@@ -1,18 +1,8 @@
 package elevenjo.ssdam.domain.user.dto;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-
 public record RegisterUserInfoRequestDto(
-        @NotBlank
         String userName,
-        @NotBlank
         String birthday,
-        @NotBlank
         String withdrawAccountNo,
-        @NotNull @Min(value = 1) @Max(value = 100)
         Integer savingRate) {
 }
--- back/ssdam/src/main/java/elevenjo/ssdam/domain/user/dto/UpdateUserRequestDto.java
+++ back/ssdam/src/main/java/elevenjo/ssdam/domain/user/dto/UpdateUserRequestDto.java
@@ -1,13 +1,7 @@
 package elevenjo.ssdam.domain.user.dto;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
 public record UpdateUserRequestDto(
-        @NotBlank String withdrawAccountNo,
-        @NotNull @Min(value = 1) @Max(value = 100)
+        String withdrawAccountNo,
         Integer savingRate
         ) {
 }
