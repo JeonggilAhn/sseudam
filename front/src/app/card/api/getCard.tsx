db395e187e377803abe953d6980fae1736740310
이시호 <harimauho@gmail.com>
2025-04-08 16:04:10 +0900

    ⚙️ chore : 카드 조회 주소 및 sse 토큰 방식 수정

--- front/src/app/card/api/getCard.tsx
+++ front/src/app/card/api/getCard.tsx
@@ -2,7 +2,7 @@ import axiosInstance from "@/utils/axiosInstance";
 
 export const GetCardInfo = async () => {
   try {
-    const response = await axiosInstance.post("/card/myCard");
+    const response = await axiosInstance.get("/card/");
     return response;
   } catch (error) {
     console.error("❌ 카드 정보 조회 실패:", error);
