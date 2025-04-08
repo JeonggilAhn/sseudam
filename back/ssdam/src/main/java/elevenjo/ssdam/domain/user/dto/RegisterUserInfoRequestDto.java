1568bae85f72ea8ca9e614909c158d2a919d8a55
이승연 <snowball@sookmyung.ac.kr>
2025-04-08 14:24:15 +0900

    👺 hotfix : NonNull 어노테이션 제거

--- back/ssdam/src/main/java/elevenjo/ssdam/domain/user/dto/RegisterUserInfoRequestDto.java
+++ back/ssdam/src/main/java/elevenjo/ssdam/domain/user/dto/RegisterUserInfoRequestDto.java
@@ -3,7 +3,7 @@ package elevenjo.ssdam.domain.user.dto;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import lombok.NonNull;
+import jakarta.validation.constraints.NotNull;
 
 public record RegisterUserInfoRequestDto(
         @NotBlank
@@ -12,6 +12,6 @@ public record RegisterUserInfoRequestDto(
         String birthday,
         @NotBlank
         String withdrawAccountNo,
-        @NonNull @Min(value = 1) @Max(value = 100)
+        @NotNull @Min(value = 1) @Max(value = 100)
         Integer savingRate) {
 }
