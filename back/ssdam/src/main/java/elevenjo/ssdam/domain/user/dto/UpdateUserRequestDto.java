e15ecce3088a835bf29fadb40868da6f0d7b84e4
조현석 <hsjo0227@gmail.com>
2025-04-08 14:36:28 +0900

    🐛 fix : @NonNull을 @NotNull로 수정 #S12P21A106-594

--- back/ssdam/src/main/java/elevenjo/ssdam/domain/user/dto/UpdateUserRequestDto.java
+++ back/ssdam/src/main/java/elevenjo/ssdam/domain/user/dto/UpdateUserRequestDto.java
@@ -3,11 +3,11 @@ package elevenjo.ssdam.domain.user.dto;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import lombok.NonNull;
+import jakarta.validation.constraints.NotNull;
 
 public record UpdateUserRequestDto(
         @NotBlank String withdrawAccountNo,
-        @NonNull @Min(value = 1) @Max(value = 100)
+        @NotNull @Min(value = 1) @Max(value = 100)
         Integer savingRate
         ) {
 }
