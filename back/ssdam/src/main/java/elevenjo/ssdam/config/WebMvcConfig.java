308a5de24c7cf6b3283e5fa697650bfa3d60c017
조현석 <hsjo0227@gmail.com>
2025-04-09 09:28:40 +0900

    🐛 fix : 일반 대기열 및 passkey 삭제 #S12P21A106-586

--- back/ssdam/src/main/java/elevenjo/ssdam/config/WebMvcConfig.java
+++ back/ssdam/src/main/java/elevenjo/ssdam/config/WebMvcConfig.java
@@ -10,21 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-	private final PasskeyInterceptor passkeyInterceptor;
-
-	@Autowired
-	public WebMvcConfig(PasskeyInterceptor passkeyInterceptor) {
-		this.passkeyInterceptor = passkeyInterceptor;
-	}
-
 	@Override
 	public void configurePathMatch(PathMatchConfigurer configurer) {
 		configurer.addPathPrefix("/api", c -> true);
 	}
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(passkeyInterceptor)
-				.addPathPatterns("/**");  // 모든 요청에 대해 인터셉터 적용 (필요시 제외할 URL 지정 가능)
-	}
 }
